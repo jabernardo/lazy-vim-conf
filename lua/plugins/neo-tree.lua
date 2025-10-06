69cc00fb6fcf5d82e03d8611574085b4958cf4dc
John Aldrich Bernardo <4ldrich@protonmail.com>
2025-10-06 17:26:00 +0800

    enable relative lines

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,13 +6,21 @@ return {
         visible = true,
         show_hidden_count = true,
         hide_dotfiles = false,
-        hide_gitignored = true,
+        hide_gitignored = false,
         hide_by_name = {
           -- '.git',
           -- '.DS_Store',
           -- 'thumbs.db',
         },
         never_show = {},
+      },
+    },
+    event_handlers = {
+      {
+        event = "neo_tree_buffer_enter",
+        handler = function()
+          vim.opt_local.relativenumber = true
+        end,
       },
     },
   },
